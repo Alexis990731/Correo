59fcb308921665d73c86580ccd006ab2758b395f
Alexis990731 <56143855+Alexis990731@users.noreply.github.com>
2021-03-21 22:50:25 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,32 +13,3 @@ app.use(express.static(path.join(__dirname, "public")))
 app.listen(3000, () =>{
     console.log("Servidor en -> http:/localhost:3000")
 })
-
-/*app.post("/send-email", (req, res) => {
-    var transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: "false",
-        auth:{
-            user : "luisa1@ethereal.email",
-            pass : "Kw3GYpMzsMkD1RXVhh"
-        }
-    })
-    
-var mailOption = {
-    from: "Remitente",
-    to: "alexisomg99@gmail.com",
-    subject: "Enviado desde nodemailer",
-    text: "hola mundo"
-}
-
-    transporter.sendMail(mailOption, (error, info) =>{
-        if(error){
-            res.status(500).send(error.message)
-        }else{
-            console.log("Email enviado")
-            res.status(200).jsonp(req.body)
-        }
-    })
-})
-*/
